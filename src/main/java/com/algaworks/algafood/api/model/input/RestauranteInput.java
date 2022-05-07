8335bf23582ba0ac6437bb0df9273628022e9771
Mksfelipe <sousafelipe123@gmail.com>
2022-05-07 20:48:33 -0300

    atualizando

--- src/main/java/com/algaworks/algafood/api/model/input/RestauranteInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/RestauranteInput.java
@@ -27,5 +27,9 @@ public class RestauranteInput {
 	@Valid
 	@NotNull
 	private CozinhaIdInput cozinha;
+	
+	@Valid
+	@NotNull
+	private EnderecoInput endereco;
 
 }
--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,13 +4,21 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.algaworks.algafood.api.model.EnderecoModel;
+import com.algaworks.algafood.domain.model.Endereco;
+
 @Configuration
 public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
-		
+
+		var enrecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+
+		enrecoToEnderecoModelTypeMap.<String>addMapping(src -> src.getCidade().getEstado().getNome(),
+				(dest, value) -> dest.getCidade().setEstado(value));
+
 		return modelMapper;
 	}
 
